8764b60615bc490377196a7247ea0582bb50dc7d
SarayRivas <ss.rivas@uniandes.edu.co>
2025-05-19 16:32:50 -0500

    .

--- src/app/vuelo/vuelo-list/vuelo-list.component.ts
+++ src/app/vuelo/vuelo-list/vuelo-list.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Vuelo } from '../vuelo';
+import { VueloService } from '../vuelo.service';
+import {VueloDetail} from '../vuelo-detail';
 
 @Component({
   selector: 'app-vuelo-list',
   templateUrl: './vuelo-list.component.html',
+  standalone: false,
   styleUrls: ['./vuelo-list.component.css']
 })
 export class VueloListComponent implements OnInit {
+  Vuelo: Array<Vuelo> = [];
+  selected: Boolean = false;
+  selectedTeatro!: VueloDetail
+  nombreVuelo: string = '';
 
-  constructor() { }
+  constructor( private vueloService: VueloService) { }
 
   ngOnInit() {
+    this.getVuelos();
   }
+
+  getVuelos() {
+    this.vueloService.getVuelos().subscribe((data: Vuelo[]) => {
+      this.Vuelo = data;
+    });
+  } 
+
+  mostrarVuelo(vuelo: Vuelo) {
+    this.nombreVuelo = vuelo.airlineName;
+  }
+
+
+    
+
+ 
 
 }
